116c4f8489e5f0275b31e80434b7bdf748fb75d9
Bman <bryantrlinares@yahoo.com>
2014-10-16 16:48:25 -0400

    first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # returns the full title on a per-page basis
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "DTrials Skeleton Framework  Oct 6 - V2.0"
     if page_title.empty?
       base_title
     else
